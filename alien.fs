cd23f413551df1128e18a05a48fa09a7702631d1
Adrian <adicirstei@gmail.com>
2016-09-02 17:23:03 +0300

    some work

--- alien.fs
+++ alien.fs
@@ -1,23 +1,36 @@
-type ConsoleInteract = (string -> string)
+type InteractCallback = (string -> string)
 
-let interact (fn:ConsoleInteract) : unit = 
+let interact (fn:InteractCallback) : unit = 
   System.Console.Out.Write(fn (System.Console.In.ReadToEnd()))
 
+ 
+let parseHeader (line: string) =
+  let tokens  = line.Split [|' '|]
+  match tokens with
+  | [|a;b;c|] -> (int a, int b, int c)
+  | _ -> failwith "invalid header"
 
+let solveTest d len n t =
+  sprintf "Case #%d: %d" n (String.length t)
 
+let join a b = 
+  a + "\n" + b
 
+let solve (input:string) = 
+  let h::tail = Array.toList (input.Split [|'\n'|])
+  
+  let (l,d,n) = parseHeader h
+  let dict = 
+    List.take d tail
+    |> Set.ofList
 
+  let tests = List.skip d tail
 
-
-
-
-
-
-
-
-
+  Seq.map2 (solveTest dict l) [1..n] tests
+  |> Seq.reduce join
+  
 
 [<EntryPoint>]
 let main _ = 
-  interact (sprintf "...%s...") |> ignore
+  interact solve
   0
