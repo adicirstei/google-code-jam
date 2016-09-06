577069ea01785b1dc7b0e9c29dc05be9e279368d
Adrian <adicirstei@gmail.com>
2016-09-06 13:39:28 +0300

    some more functions

--- welcome.fs
+++ welcome.fs
@@ -1,3 +1,29 @@
+let toSeq s =
+  seq {for c in s -> c}
+
+let combineTwo a b =
+  [for i in a do 
+    for j in b do 
+      if i < j then yield (i,j)]
+
+
+
+
+let findAll (input:string) = 
+  let searchString = [for c in "welcome to code jam" -> c ]
+  let idxOf c =
+    input
+    |> toSeq
+    |> Seq.mapi (fun i c -> (i,c))
+    |> Seq.filter (fun (_, x) -> c = x)
+    |> Seq.map fst
+
+  searchString
+  |> List.map idxOf
+  
+
+findAll "wellcoome to coodee jamm"
+
 
 let interact (fn:(string -> string)) : int = 
   System.Console.Out.Write(fn (System.Console.In.ReadToEnd()))
@@ -11,6 +37,10 @@ module String =
 let solve i = 
   let (_ :: data) = String.Split '\n' i
   
+
+
+
+
 
 
 
